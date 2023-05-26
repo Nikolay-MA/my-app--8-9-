b79ec313b2e9b791f66b07449c6545049b6ba8d0
Nikolay-MA <nikolay02092008@gmail.com>
2023-05-26 22:04:23 +0300

    ок

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,35 @@ import Projects from "./Projects/Projects";
 // import Domd3 from "./Domd3/Domd3";
 import NavBar from "./NavBar/NavBar";
 import AboutMe from "./AboutMe/AboutMe";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Reg from "./Reg/Reg";
 import { CookiesProvider } from 'react-cookie';
 import { useCookies } from 'react-cookie';
 import Me from "./Me/Me";
 function App() {
   const [cookie, setCookie, removeCookie] = useCookies(['Name']);
-  const [Name1, setName1] = useState({ Name2: cookie.Name })
   const Navigate = useNavigate()
   useEffect(() => {
 
     // setName1({ Name2: cookie.Name })
-    if (Name1.Name2 === 'undefined' || Name1.Name2 === '' || Name1.Name2 === undefined) {
-      // console.log(Name1.Name2);
+    if (cookie.Name === 'undefined' || cookie.Name === '' || cookie.Name === undefined) {
+      // console.log(cookie.Name);
       Navigate('/Reg')
       // console.log(cookie.Name);
     } else{
       Navigate('/Me')
+      console.log(1);
     }
   }, [])
   return (
     <>
-      {cookie.Name && <NavBar removeCookie={removeCookie} cookie={Name1.Name2} />}
+      {cookie.Name && <NavBar removeCookie={removeCookie} cookie={cookie.Name} />}
       <Routes>
         <Route path="/">
           <Route path="AboutMe" element={<AboutMe/>} />
           <Route path="Projects/*" element={<Projects />} />
-          <Route path="Me" element={<Me cookie={Name1.Name2}/>} />
-          <Route path="Reg" element={<CookiesProvider><Reg cookie={cookie} Name1={Name1} setName1={setName1} setCookie={setCookie} /></CookiesProvider>} />
+          <Route path="Me" element={<Me cookie={cookie.Name}/>} />
+          <Route path="Reg" element={<CookiesProvider><Reg cookie={cookie} setCookie={setCookie} /></CookiesProvider>} />
         </Route>
       </Routes>
     </>
--- src/Reg/Reg.jsx
+++ src/Reg/Reg.jsx
@@ -1,30 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 import { useEffect } from "react";
-const Reg = ({ cookie, Name1, setName1, setCookie }) => {
+const Reg = ({ cookie,  setCookie }) => {
     const navigate = useNavigate()
     const loadhandler = (event) => {
         event.preventDefault();
         setCookie('Name', event.target[0].value, { path: '/' })
-        // setName1({ Name2: cookie.Name })
-        console.log(cookie.Name);
-        console.log(Name1.Name2);
-        if (Name1.Name2 != 'undefined' && Name1.Name2 != undefined) {
-            navigate('/Me')
-            console.log(1);
-        } else {
-            if (cookie.Name != 'undefined' && cookie.Name != undefined) {
-                setName1({ Name2: cookie.Name })
-                navigate('/Me')
-                console.log(2);
-            }
-        }
     }
     useEffect(() => {
-        setName1({ Name2: cookie.Name })
-        console.log(3);
-        console.log(cookie.Name)
-      }, [cookie.Name])
+        // setName1({ Name2: cookie.Name })
+        console.log(cookie.Name);
+
+        if (cookie.Name != 'undefined' && cookie.Name != undefined && cookie.Name != '') {
+            // setName1({ Name2: cookie.Name })
+            navigate('/Me')
+        }
+    }, [cookie.Name])
     return (
         <>
             <form onSubmit={loadhandler}>
